40123d0cd64cacdc6944d476c7ec7603b96ad583
Velvid <muntakim.ca@gmail.com>
2022-05-22 17:54:46 -0400

    removing narrow namespace from print_utility

--- src/print_utility.cpp
+++ src/print_utility.cpp
@@ -1,49 +1,6 @@
 #include "print_utility.hpp"
 
-namespace draw::narrow
-{
-    static std::string row(const uint8_t index, const std::array<uint8_t, 9>& numbers)
-    {
-        std::string ret {};
-
-        ret += std::to_string(index + 1) + " | ";
-
-        for (auto i = 0; i < 9; ++i)
-        {
-            ret += ((numbers[i] != 0) ? std::to_string(numbers[i]) : "-");
-
-            if ((i + 1) % 3 == 0)
-                ret += " |";
-
-            ret += " ";
-        }
-
-        ret += std::to_string(index + 1) + "\n";
-
-        return ret;
-    }
-}
-
-std::ostream& operator<<(std::ostream& out, const grid& board)
-{
-    out <<  draw::narrow::alpha;
-    out <<  draw::narrow::middle;
-
-    for (auto i = 0; i < 9; ++i)
-    {
-        out <<  draw::narrow::row(i, board[i]);
-
-        if (((i + 1) % 3 == 0) && (i != 0 && i != 8))
-            out <<  draw::narrow::middle;
-    }
-
-    out <<  draw::narrow::middle;
-    out <<  draw::narrow::alpha;
-
-    return out;
-}
-
-namespace draw::wide
+namespace draw
 {
     static std::wstring row(const uint8_t index, const std::array<uint8_t, 9>& numbers)
     {
@@ -53,7 +10,10 @@ namespace draw::wide
 
         for (auto i = 0; i < 9; ++i)
         {
-            ret += ((numbers[i] != 0) ? std::to_wstring(numbers[i]) : L"-");
+            if (numbers[i] != 0)
+                ret += std::to_wstring(numbers[i]);
+            else
+                ret += L"-";
 
             if ((i + 1) % 3 == 0)
                 ret += L" |";
@@ -69,19 +29,19 @@ namespace draw::wide
 
 std::wostream& operator<<(std::wostream& out, const grid& board)
 {
-    out <<  draw::wide::alpha;
-    out <<  draw::wide::top;
+    out <<  draw::alpha;
+    out <<  draw::top;
 
     for (auto i = 0; i < 9; ++i)
     {
-        out <<  draw::wide::row(i, board[i]);
+        out <<  draw::row(i, board[i]);
 
         if (((i + 1) % 3 == 0) && (i != 0 && i != 8))
-            out <<  draw::wide::middle;
+            out <<  draw::middle;
     }
 
-    out <<  draw::wide::bottom;
-    out <<  draw::wide::alpha;
+    out <<  draw::bottom;
+    out <<  draw::alpha;
 
     return out;
 }
